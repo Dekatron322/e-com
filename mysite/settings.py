4a5b23a773d651cf6a1eefbab63170741b809fb3
Ebraheem <muritalaibrahim097@gmail.com>
2020-07-08 22:35:41 +0100

    first-commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2jn-095e(25-k*is615wdo928gu+70vx!u5wm&$+@*gk#j@!2='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000", "tundett.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "tundett.herokuapp.com"]
 
 
 # Application definition
